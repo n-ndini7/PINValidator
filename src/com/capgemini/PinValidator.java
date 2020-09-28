ab4fc418f5f04e4080fe9f876e51ca499b639ed8
n-ndini7 <hp@RJs-PC>
2020-09-28 22:48:07 +0530

    [Nandini] added master - welcome to PIN validator

---
+++ src/com/capgemini/PinValidator.java
@@ -0,0 +1,8 @@
+package com.capgemini;
+
+public class PinValidator {
+//Master branch - welcome message
+	public static void main(String[] args) {
+		System.out.println("Welcome to PIN code validator program!!");
+	}
+}
